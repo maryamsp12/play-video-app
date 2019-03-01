9daaf8f7121d9087ed4a8de39cae2dc386fcbc3a
maryamsp12 <maryamsp12@example.com>
2019-03-01 20:54:51 +0200

    Updates

---
+++ precache-manifest.737fa4176b14b90b44b94e641fc825a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bfa14aebabb907d4ff9",
+    "url": "/play-video-app/static/js/runtime~main.2bfa14ae.js"
+  },
+  {
+    "revision": "8e1b5321f2ce27d4fe86",
+    "url": "/play-video-app/static/js/main.8e1b5321.chunk.js"
+  },
+  {
+    "revision": "e73551dc88978be622b4",
+    "url": "/play-video-app/static/js/2.e73551dc.chunk.js"
+  },
+  {
+    "revision": "8e1b5321f2ce27d4fe86",
+    "url": "/play-video-app/static/css/main.2bb6d3c9.chunk.css"
+  },
+  {
+    "revision": "6b6530cbe118706d30a9232e012fe2f4",
+    "url": "/play-video-app/index.html"
+  }
+];
